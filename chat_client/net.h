687391d9e94ba7ec0a38b6d7ec09c5385c3cdf8b
Ekaterian Sukhanova <102546155+EkaSukh@users.noreply.github.com>
2022-09-08 20:52:27 +0300

    Create net.h

---
+++ chat_client/net.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <unistd.h>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cmath>
+
+class ClientTCP
+{
+    public:
+
+        ClientTCP();
+        ClientTCP(std::string &ip);
+        ~ClientTCP() {}
+        
+        //client server comunication
+        void clientConnect();
+        void clientClose() {close(sockd);}
+
+        //process information in client server comunication
+        void readInput();
+        int readBuffer(int start, std::string &outcome, char s = '\0');
+        char atBuff(int n);
+        int writeBuffer(int start, const std::string &outcome, char s = '\0');
+        void clearBuffer();
+        void writeOutput();
+
+        //helping fuctions
+        int transformInNumber(std::string& str_num);
+        void showBuff(const int n);
+
+    private:
+        const static int BUF_SIZE = 1024;
+        const static int PORT = 7777;
+        
+        int sockd, connection;
+        int buff_shift;
+        struct sockaddr_in serveraddress, client;
+        char buff[BUF_SIZE];
+        socklen_t length;
+        
+};
